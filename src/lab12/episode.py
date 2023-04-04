1a46bf523a7c58d817da33a9c857211db697adec
Stefan Milinkovic <skm6331@psu.edu>
2023-04-04 10:52:45 -0400

    4/4

--- src/lab12/episode.py
+++ src/lab12/episode.py
@@ -1,3 +1,4 @@
+import random
 ''' 
 Lab 12: Beginnings of Reinforcement Learning
 We will modularize the code in pygrame_combat.py from lab 11 together.
@@ -11,3 +12,45 @@ Note that observation/state is a tuple of the form (player1_health, player2_heal
 Action is simply the weapon selected by the player.
 Reward is the reward for the player for that turn.
 '''
+# return health of players, action, reward 
+
+
+def run_episode(player1, player2):
+    # Initialize the players' health
+    player1_health = 100
+    player2_health = 100
+
+    # Initialize the list to hold the episode history
+    episode_history = []
+
+    # Define the weapons that each player can use
+    weapons = ["Sword", "Bow", "Fire"]
+
+    # Run the episode until one player's health reaches 0
+    while player1_health > 0 and player2_health > 0:
+        # Determine which player is attacking and which is defending
+        if random.random() < 0.5:
+            attacker = player1
+            defender = player2
+        else:
+            attacker = player2
+            defender = player1
+
+        weapon = attacker.select_weapon(weapons)
+        damage = attacker.calculate_damage(weapon)
+        defender_health = defender.take_damage(damage)
+        reward = attacker.calculate_reward(defender_health)
+
+        # Add the current state, action, and reward to the episode history
+        state = (player1_health, player2_health)
+        action = weapon
+        episode_history.append((state, action, reward))
+
+        # Update the players' health
+        if attacker == player1:
+            player2_health = defender_health
+        else:
+            player1_health = defender_health
+
+    # Return the episode history
+    return episode_history
